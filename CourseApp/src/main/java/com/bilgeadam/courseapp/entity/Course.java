3d92073fa38d042cfc226ca0ed7540aef4c222a9
mtcrk <36543546+mtcrk@users.noreply.github.com>
2022-01-09 12:49:41 +0300

    son

---
+++ CourseApp/src/main/java/com/bilgeadam/courseapp/entity/Course.java
@@ -0,0 +1,15 @@
+package com.bilgeadam.courseapp.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Course {
+    private String courseId;
+    private String courseName;
+    private String authorName;
+
+}
